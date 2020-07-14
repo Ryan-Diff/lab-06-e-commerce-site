19ff09069b5912e435bc786e8adedda16e7b737a
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-07-14 08:49:54 -0700

    Made product list and added JS

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 // import functions and grab DOM elements
-
+import { renderWhiskey } from './render-whisky.js';
+import { whiskeyList } from './whiskey.js';
 // initialize state
-
+const whiskeyDiv = document.getElementById('whiskey');
 // set event listeners to update state and DOM
+for (let i = 0; i < whiskeyList.length; i++) {
+    const booze = whiskeyList[i];
+    const el = renderWhiskey(booze);
+    whiskeyDiv.appendChild(el);
+}
---
+++ whiskey.js
@@ -0,0 +1,45 @@
+const jameson = {
+    id: 'jameson',
+    name: 'Jameson Whiskey',
+    image: 'apple.png',
+    description: 'Smooth with hints of vanilla.',
+    category: 'Irish',
+    price: 18.00,
+    cost: 12.00
+};
+const lagavulin = {
+    id: 'lagavulin',
+    name: 'Lagavulin Scotch Whiskey',
+    image: 'apple.png',
+    description: 'Thick and rich. Malty with a fruity sweetness. Long smokey and spicy finish.',
+    category: 'Scotch',
+    price:80.00,
+    cost: 50.00
+};
+const rittenhouse = {
+    id: 'rittenhouse',
+    name: 'Rittenhouse Rye Whiskey',
+    image: 'apple.png',
+    description: 'Sweet start with a transition to dark and roasty flavors. Oak and honey finish.',
+    category: 'Rye',
+    price: 35.00,
+    cost: 19.00
+};
+const angelsEnvy = {
+    id: 'angels',
+    name: 'Angels Envy Bourbon',
+    image: 'apple.png',
+    description: 'Undertones of smoke. Vanilla, Maple Syrup, Toasted Nuts and tangerine',
+    category: 'bourbon',
+    price: 100.00,
+    cost: 85.00
+};
+const balvenie = {
+    id: 'balvenie',
+    name: 'Balvenie 50',
+    image: 'apple.png',
+    description: 'Citrus and oak on the nose. Warm spice and honey. Sweet citrus finish.',
+    category: 'scotch',
+    price: 55000.00,
+    cost: 40000.00
+};
